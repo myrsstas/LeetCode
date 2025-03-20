1ac92948a7828887acff1b711a0660d7a3cfd61a
Myrsini Stasinou <myrsinistas@gmail.com>
2025-03-21 01:08:56 +0200

    Completed Merged strings by adding letters in alternating order Solution

---
+++ LeetCode/LeetCode/Arrays_Strings/MergeAlternatelySolution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode.Arrays_Strings;
+
+/*
+ * You are given two strings word1 and word2. Merge the strings by adding letters in alternating order,
+ * starting with word1. If a string is longer than the other, append the additional
+ * letters onto the end of the merged string.
+ * Return the merged string.
+ */
+
+public class MergeAlternatelySolution
+{
+    public string MergeAlternately(string word1, string word2) {
+        var lengthWord1 = word1.Length;
+        var lengthWord2 = word2.Length;
+        int i=0, j=0;
+        var result = "";
+        while (i<lengthWord1 || j<lengthWord2){
+            if(i<lengthWord1){
+                result = result + word1[i++];
+            }
+
+            if (j<lengthWord2){
+                result = result + word2[j++];
+            }
+        }
+
+        return result;
+    }
+}
+
