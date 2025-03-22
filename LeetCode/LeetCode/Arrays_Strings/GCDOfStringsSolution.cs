fb99acc497bce05dddf0cd96048c6d18fdf09bdc
Myrsini Stasinou <myrsinistas@gmail.com>
2025-03-23 00:13:41 +0200

    Completed Greatest Common Divisor Of Strings Solution

---
+++ LeetCode/LeetCode/Arrays_Strings/GCDOfStringsSolution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode.Arrays_Strings;
+
+/*
+ * For two strings s and t, we say "t divides s" if and only if s = t + t + t + ... + t + t
+ * (i.e., t is concatenated with itself one or more times).
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+Example1 :
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+
+Example 2:
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+ */
+
+public class GCDOfStringsSolution
+{
+    public string GcdOfStrings(string str1, string str2)
+    {
+        if ((str1 + str2).Equals(str2 + str1))
+        {
+            return str1.Substring(0, gcd(str1.Length, str2.Length));
+        }
+        else
+        {
+            return "";
+        }
+    }
+
+    private int gcd(int str1, int str2)
+    {
+        if (str2 == 0)
+        {
+            return str1;
+        }
+
+        return gcd(str2, str1 % str2);
+    }
+}
--- LeetCode/LeetCode/Arrays_Strings/MergeAlternatelySolution.cs
+++ LeetCode/LeetCode/Arrays_Strings/MergeAlternatelySolution.cs
@@ -5,21 +5,33 @@
  * starting with word1. If a string is longer than the other, append the additional
  * letters onto the end of the merged string.
  * Return the merged string.
+ *
+ * Example :
+Input: word1 = "abc", word2 = "pqr"
+Output: "apbqcr"
+Explanation: The merged string will be merged as so:
+word1:  a   b   c
+word2:    p   q   r
+merged: a p b q c r
  */
 
 public class MergeAlternatelySolution
 {
-    public string MergeAlternately(string word1, string word2) {
+    public string MergeAlternately(string word1, string word2)
+    {
         var lengthWord1 = word1.Length;
         var lengthWord2 = word2.Length;
-        int i=0, j=0;
+        int i = 0, j = 0;
         var result = "";
-        while (i<lengthWord1 || j<lengthWord2){
-            if(i<lengthWord1){
+        while (i < lengthWord1 || j < lengthWord2)
+        {
+            if (i < lengthWord1)
+            {
                 result = result + word1[i++];
             }
 
-            if (j<lengthWord2){
+            if (j < lengthWord2)
+            {
                 result = result + word2[j++];
             }
         }
@@ -27,4 +39,3 @@ public class MergeAlternatelySolution
         return result;
     }
 }
-
